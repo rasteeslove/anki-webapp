2101ec992a917d894c999d9badbff6d2c0a2807a
krastsislau <krastsislau@gmail.com>
2022-05-11 16:10:00 +0300

    + auth feature, routes, and docu

--- anki/src/App.tsx
+++ anki/src/App.tsx
@@ -1,4 +1,10 @@
 import { useContext } from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 import Header from 'components/Header'
 import { ThemeContext } from 'context/ThemeContext'
 
@@ -16,9 +22,24 @@ const App = () => {
             flexFlow: 'column',
             transition: 'background-color 100ms'
         }}>
+            {/*
+                ok, so some docu:
+                the header is always there; the status bar probably too ?
+                then there's routing:
+                - if the URL is invalid, redirect to auth if not auth'd, else to the auth'd user's decks,
+                  except when the URL is a subURL of a valid link, then go to the latter
+                - /auth: auth, obviously
+                - /{username}: {username}'s deckspace, might be yours (if not, showing only public decks)
+                - /{username}/{deckname}: accessible deckinfo open over the owner's deckspace with buttons for allowed actions
+                - /{username}/{deckname}/train: train mode on an accessible deck; if you're auth'd, your stats will be persisted
+                - /{username}/{deckname}/edit: edit mode on your deck
+            */}
             <Header/>
-
-            <DeckSpace/>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<DeckSpace/>}/>
+                </Routes>
+            </Router>
         </div>
     )
 }
