b0f9de700ca93911f12ba00eb91c675284d1645e
krastsislau <krastsislau@gmail.com>
2022-06-08 16:56:15 +0300

    rmved old functions for cards retrieval

--- anki/src/features/deckspace/components/DeckSpace.tsx
+++ anki/src/features/deckspace/components/DeckSpace.tsx
@@ -20,50 +20,11 @@ const DeckSpace = () => {
     const [decks, setDecks] = useState<Array<DeckType>>([]);
     const [deckSelected, setDeckSelected] = useState(false);
 
-    /*\
-    let decks = [
-        { 'color': '#A183C7', 'name': 'Mandarin' },
-        { 'color': '#94A4F5', 'name': 'Linear Algebra I' },
-        { 'color': '#FCB778', 'name': 'Math Statistics I' },
-        { 'color': '#88FFCD', 'name': 'Linear Algebra II' },
-        { 'color': '#F59A94', 'name': 'Calculus' },
-        { 'color': '#F594C3', 'name': 'Spanish' },
-        { 'color': '#69C578', 'name': 'Philosophy I' },
-        { 'color': '#D5DE6C', 'name': 'History I' },
-        { 'color': '#87DAE5', 'name': 'Math Statistics II' },
-        { 'color': '#A183C7', 'name': 'Mandarin' },
-        { 'color': '#94A4F5', 'name': 'Linear Algebra I' },
-        { 'color': '#FCB778', 'name': 'Math Statistics I' },
-        { 'color': '#88FFCD', 'name': 'Linear Algebra II' },
-        { 'color': '#F59A94', 'name': 'Calculus' },
-        { 'color': '#F594C3', 'name': 'Spanish' },
-        { 'color': '#69C578', 'name': 'Philosophy I' },
-        { 'color': '#D5DE6C', 'name': 'History I' },
-        { 'color': '#87DAE5', 'name': 'Math Statistics II' },
-    ]
-    \*/
-
     // todo: decks to be retrieved from API using current URL username fragment.
     // if invalid, perform redirect to auth / auth'd user deckspace
     useEffect(() => {
         getDecks('krastsislau').then(data => setDecks(data));
     }, []);
-
-    /*\
-    const getDecks = () => {
-        const deckComponents = []
-        for (let i = 0; i < decks.length; i++) {
-            deckComponents.push(
-                <Deck color={decks[i]['color']}
-                      name={decks[i]['name']}
-                      onClick={() => {
-                          setDeckSelected(true)
-                      }} />
-            )
-        }
-        return deckComponents
-    }
-    \*/
 
     return(
         <div className="deck-space-and-status-bar">
