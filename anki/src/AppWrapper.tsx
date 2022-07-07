c8f6220593696a2566f1a4e3767d6d0a11e11ddf
krastsislau <krastsislau@gmail.com>
2022-07-07 18:23:00 +0400

    added UserContext to AppWrapper

--- anki/src/AppWrapper.tsx
+++ anki/src/AppWrapper.tsx
@@ -2,15 +2,22 @@ import { useState } from 'react';
 import { App } from 'App';
 import { ThemeContext, themes } from 'context/ThemeContext';
 import { MinimalismContext } from 'context/MinimalismContext';
+import { UserContext } from 'context/UserContext';
 import { storage } from 'utils/storage';
 
 const AppWrapper = () => {
     const [theme, setTheme] = useState(themes[storage.getTheme()]);
+    const [user, setUser] = useState(null);
+    const isUserLoggedIn = () => {
+        return !!user;
+    }
 
     return (
         <ThemeContext.Provider value={[theme, setTheme]}>
             <MinimalismContext.Provider value={false}>
-                <App/>
+                <UserContext.Provider value={{user, setUser, isUserLoggedIn}}>
+                    <App/>
+                </UserContext.Provider>
             </MinimalismContext.Provider>
         </ThemeContext.Provider>
     );
