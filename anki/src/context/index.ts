b3d30f477eafe72c1e874febeffd86b04e9b34af
krastsislau <krastsislau@gmail.com>
2022-07-12 16:55:49 +0400

    added a couple of index files for better imports

---
+++ anki/src/context/index.ts
@@ -0,0 +1,5 @@
+import { MinimalismContext } from "./MinimalismContext";
+import { ThemeContext } from "./ThemeContext";
+import { UserContext } from "./UserContext";
+
+export { MinimalismContext, ThemeContext, UserContext };
