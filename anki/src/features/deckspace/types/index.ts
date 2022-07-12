c498ee9cf8a4edeca9de6243c91ebe6b2e6da4b4
krastsislau <krastsislau@gmail.com>
2022-07-12 17:01:10 +0400

    deckspace separate types directory created

---
+++ anki/src/features/deckspace/types/index.ts
@@ -0,0 +1,16 @@
+export type DeckType = {
+    id: number,
+    name: string,
+    color: string,
+    public: boolean,
+    owner: number,
+};
+
+export type DeckInfoType = {
+    id: number,
+    name: string,
+    color: string,
+    public: boolean,
+    owner: number,
+    description: string,
+};
