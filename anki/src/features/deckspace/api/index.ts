303487e40d72c01670d47ef43574fe223b27f1e8
krastsislau <krastsislau@gmail.com>
2022-07-14 17:37:29 +0400

    rmved redundancy from api functions in deckspace

--- anki/src/features/deckspace/api/index.ts
+++ anki/src/features/deckspace/api/index.ts
@@ -1,16 +1,15 @@
 import { axios } from 'lib/axios';
-import { API_URL } from 'config';
 
 const getDecks = async (username: string) => {
     return axios
-        .get(`${API_URL}/get-decks`, {
+        .get('/get-decks', {
             params: { username },
         }).then(res => res.data);
 };
 
 const getDeckInfo = async (username: string, deckname: string) => {
     return axios
-        .get(`${API_URL}/get-deck-info`, {
+        .get('/get-deck-info', {
             params: { username, deckname },
         }).then(res => res.data);
 };
