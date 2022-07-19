a6665454de642714b90fe54e980a736ab24c2664
krastsislau <krastsislau@gmail.com>
2022-07-19 19:05:36 +0400

    deckinfo description styled for raw text

--- anki/src/features/deckspace/components/DeckInfo.tsx
+++ anki/src/features/deckspace/components/DeckInfo.tsx
@@ -34,7 +34,7 @@ const DeckInfo = () => {
         }}>
             <div className='shadow-in-top-light inner-shadow-maker'/>
             <div className="deckname-holder">
-                {deckInfo?.name}
+                { deckInfo && deckInfo.name }
             </div>
             <div className="mode-buttons-holder">
                 <ButtonSwitch is_on={true} text={'description'}
@@ -48,45 +48,7 @@ const DeckInfo = () => {
             </div>
             <div className="deckinfo-main">
                 <div className="deckinfo-description-box">
-                    { /* the stuff below gonna stay this way for some more time 
-                         before I start testing descriptions */ }
-                    This is to be a description box of sorts, I imagine. By default it is to be something like “(no description)” but italic because I want it to be so. The user can also write something meaningful here in order to define the purpose of the deck. I imagine this box would also support markdown and stuff but it’s not compulsory. This is just a description after all.
-                    <br/>
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium varius massa. Aliquam erat volutpat. Maecenas vel justo a diam finibus gravida. Curabitur cursus, erat ac sodales consectetur, augue diam cursus tellus, et consectetur diam felis vel urna.
-                    <br/>
-                    <br/>
-                    The box is to be scrollable too.
-                    <br/>
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium varius massa. Aliquam erat volutpat. Maecenas vel justo a diam finibus gravida. Curabitur cursus, erat ac sodales consectetur, augue diam cursus tellus, et consectetur diam felis vel urna.
-                    <br/>
-                    <br/>
-                    The box is to be scrollable too.
-                    <br/>
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium varius massa. Aliquam erat volutpat. Maecenas vel justo a diam finibus gravida. Curabitur cursus, erat ac sodales consectetur, augue diam cursus tellus, et consectetur diam felis vel urna.
-                    <br/>
-                    <br/>
-                    The box is to be scrollable too.
-                    <br/>
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium varius massa. Aliquam erat volutpat. Maecenas vel justo a diam finibus gravida. Curabitur cursus, erat ac sodales consectetur, augue diam cursus tellus, et consectetur diam felis vel urna.
-                    <br/>
-                    <br/>
-                    The box is to be scrollable too.
-                    <br/>
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium varius massa. Aliquam erat volutpat. Maecenas vel justo a diam finibus gravida. Curabitur cursus, erat ac sodales consectetur, augue diam cursus tellus, et consectetur diam felis vel urna.
-                    <br/>
-                    <br/>
-                    The box is to be scrollable too.
-                    <br/>
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium varius massa. Aliquam erat volutpat. Maecenas vel justo a diam finibus gravida. Curabitur cursus, erat ac sodales consectetur, augue diam cursus tellus, et consectetur diam felis vel urna.
-                    <br/>
-                    <br/>
-                    The box is to be scrollable too.
+                    { !!deckInfo && deckInfo.description }
                 </div>
             </div>
             <div className="bottom-clippy" style={{
