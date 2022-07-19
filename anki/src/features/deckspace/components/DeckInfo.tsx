5a33019f06ddcdb45b5040b29f2a1be3d7229f49
krastsislau <krastsislau@gmail.com>
2022-07-19 22:14:29 +0400

    codestyle minor change

--- anki/src/features/deckspace/components/DeckInfo.tsx
+++ anki/src/features/deckspace/components/DeckInfo.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from "react";
-import {Link, useNavigate, useParams} from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { ThemeContext } from "context";
 import { getDeckInfo } from "../api";
