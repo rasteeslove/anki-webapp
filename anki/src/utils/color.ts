3422357b143aeea1aea9c94dec2c47a7f294d341
krastsislau <krastsislau@gmail.com>
2022-07-20 20:58:21 +0400

    declared a couple of helper util functions

---
+++ anki/src/utils/color.ts
@@ -0,0 +1,14 @@
+/*\
+This file contains helper functions to better manage
+and display colors.
+
+e.g.:
+    text color (black/white) depending on background color:
+        black -> white
+        white -> black
+        yellow -> black
+        very light blue -> black
+        dark red -> white
+\*/
+
+export {};
---
+++ anki/src/utils/text.ts
@@ -0,0 +1,12 @@
+/*\
+This file contains helper functions for text.
+
+e.g.:
+    n objects:
+        0 -> 0 objects
+        1 -> 1 object
+        3 -> 3 objects
+        21 -> 21 objects
+\*/
+
+export {};
