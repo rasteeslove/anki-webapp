3f74dbf1b6b87ba1f295f036fb88c4dd4bb64163
krastsislau <krastsislau@gmail.com>
2022-07-22 14:49:16 +0400

    added deployed backend support with env variable use

--- anki/src/config/index.ts
+++ anki/src/config/index.ts
@@ -1,1 +1,8 @@
-export const API_URL = 'http://localhost:8000/api';
+let API_URL: string;
+if (process.env.API_URL) {
+    API_URL = process.env.API_URL;
+} else {
+    API_URL = 'http://localhost:8000/api';
+}
+
+export { API_URL };
