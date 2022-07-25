04ccc22af5b8a27bc24efd8d9088d6bea9d15c5a
krastsislau <krastsislau@gmail.com>
2022-07-25 17:46:09 +0400

    trainmode completed but I would replace using propmt with a mini window

--- anki/src/features/trainmode/api/index.ts
+++ anki/src/features/trainmode/api/index.ts
@@ -5,16 +5,21 @@ const pullNextCard = async (deckOwnerUsername: string,
                             deckname: string) : Promise<CardType> => {
     return axios
         .get('/pull-next-card', {
-            params: { deckOwnerUsername, deckname },
+            params: {
+                deck_owner_username: deckOwnerUsername,
+                deckname
+            },
         }).then(res => res.data);
 };
 
 const postFeedback = async (deckOwnerUsername: string,
                             deckname: string,
+                            cardId: number,
                             feedback: boolean) => {
     return axios
         .post('/post-feedback/', {
             deck_owner_username: deckOwnerUsername,
+            card_id: cardId,
             deckname, feedback
         }).then(res => res.data);
 };
--- anki/src/features/trainmode/components/TrainMode.tsx
+++ anki/src/features/trainmode/components/TrainMode.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import { CardType } from "types";
 import { ThemeContext } from "context/ThemeContext";
 import { StatusBar } from "components/StatusBar";
 
@@ -12,7 +13,11 @@ import "./TrainMode.css";
 const TrainMode = () => {
     const { username, deckname } = useParams();
     const [theme, ] = useContext(ThemeContext);
+    const [totalCardNumber, setTotalCardNumber] = useState<number>(0);
     const [cardNumber, setCardNumber] = useState<number>(0);
+    const [cardInfo, setCardInfo] = useState<CardType | undefined>(undefined);
+    const [showAnswer, setShowAnswer] = useState<boolean>(false);
+    const [completed, setCompleted] = useState<boolean>(false);
 
     useEffect(() => {
         let cardNum = 0;
@@ -23,27 +28,102 @@ const TrainMode = () => {
             }
         } while (cardNum <= 0);
         setCardNumber(cardNum);
+        setTotalCardNumber(cardNum);
     }, []);
 
     useEffect(() => {
-        pullNextCard(username!, deckname!).then(() => {
+        if (cardNumber > 0) {
+            pullNextCard(username!, deckname!).then((data) => {
+                setCardInfo(data);
+            });
+        } else {
+            if (totalCardNumber > 0) {
+                setCompleted(true);
+            }
+        }
 
-        });
     }, [username, deckname, cardNumber]);
 
     return(
         <div className="trainmode-and-status-bar">
             <StatusBar status={`the "${deckname}" deck: training mode`}/>
             <div className="trainmode">
-                <ProgressBar current={4} total={14}/>
-                <div className='shadow-out-bottom question-answer' style={{
-                    backgroundColor: theme.middleground,
-                    color: theme.text,
-                }}>
+                {
+                    !completed && cardInfo &&
+                    <>
+                        <ProgressBar current={totalCardNumber-cardNumber} total={totalCardNumber}/>
+                        <div className='shadow-out-bottom question-answer' style={{
+                            backgroundColor: theme.middleground,
+                            color: theme.text,
+                        }}>
+                            <div className='question-container'>
+                                {cardInfo.question}
+                            </div>
+                            { /* TODO: maybe add visual separator */ }
+                            <div className='answer-container'>
+                                {
+                                    showAnswer ?
+                                        <>
+                                            {cardInfo.answer}
+                                        </>
+                                        :
+                                        <>
+                                            <ButtonSwitch text='show answer'
+                                                          is_on={false}
+                                                          width={160} height={40} fontSize={16}
+                                                          onClick={() => setShowAnswer(true)}/>
+                                        </>
+                                }
+                            </div>
+                            <div className='feedback-container'>
+                                {
+                                    showAnswer &&
+                                    <>
+                                        <ButtonSwitch text={'knew it'}
+                                                      is_on={false}
+                                                      color='#7CFF3E'
+                                                      width={160} height={40}
+                                                      onClick={() => {
+                                                          postFeedback(username!, deckname!,
+                                                              cardInfo.id, true)
+                                                              .then(() => {
+                                                                  setCardNumber(cardNumber-1);
+                                                                  setShowAnswer(false);
+                                                              });
+                                                      }}/>
+                                        <ButtonSwitch text={'not there yet'}
+                                                      is_on={false}
+                                                      color='#FF603E'
+                                                      width={160} height={40}
+                                                      onClick={() => {
+                                                          postFeedback(username!, deckname!,
+                                                              cardInfo.id, false)
+                                                              .then(() => {
+                                                                  setCardNumber(cardNumber-1);
+                                                                  setShowAnswer(false);
+                                                              });
+                                                      }}/>
+                                    </>
+                                }
 
-
-                    
-                </div>
+                            </div>
+                        </div>
+                    </>
+                }
+                {
+                    completed &&
+                    <div className='completed-container shadow-out-bottom' style={{
+                        backgroundColor: theme.middleground,
+                        color: theme.text,
+                    }}>
+                        <div className='completed-main'>
+                            Good job! You have trained on {totalCardNumber} card(s).
+                            <br/>
+                            <br/>
+                            <Link to={`/${username}/${deckname}`}>Back to deck</Link>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     )
