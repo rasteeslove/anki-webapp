fb93084d0590dc6b4995b52ac13222c8c56b4aa9
krastsislau <krastsislau@gmail.com>
2022-08-13 23:14:00 +0300

    rmved unused fields from user type dto

--- anki/src/types/index.ts
+++ anki/src/types/index.ts
@@ -6,18 +6,10 @@ NOTE: might need to remove unnecessary details from UserType.
 
 export type UserType = {
     id: number,
-    password: string,
     last_login: string,
-    is_superuser: boolean,
     username: string,
-    first_name: string,
-    last_name: string,
     email: string,
-    is_staff: boolean,
-    is_active: boolean,
     date_joined: string,
-    groups: Array<string>,
-    user_permissions: Array<string>,
 };
 
 export * from './errors';
